937d3bb481a99649f05587221aeeee08124ff006
roman-lopez <roman.lopez@udc.es>
2023-11-20 18:36:14 +0100

    first commit

---
+++ src/test/java/gei/id/tutelado/AllTests.java
@@ -0,0 +1,11 @@
+package gei.id.tutelado;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ P01_Usuarios.class, P02_Usuarios_Entradas.class, P03_Consultas.class } )
+public class AllTests {
+
+}
